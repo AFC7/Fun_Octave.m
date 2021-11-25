91b9f4dae5ba68f87496fe59c99c33cc7a369977
AFC7 <93738429+AFC7@users.noreply.github.com>
2021-11-25 14:35:30 -0500

    potencia, funcion, distancia,espacio libre
    potencia recibida en funcion de la distancia en el espacio libre

---
+++ pot_recibida_en_fun_distancia_espacio_libre.m
@@ -0,0 +1,22 @@
+% Pr(d) potencia recibida en funcion de la distancia en el espacio libre
+%
+%usage: Pr = (d,Pt,Gt,Gr,L,lambda)
+%
+%d= distancia en metros transmisor receptor 
+%Pt= potencia del transmisor
+%Gt= ganancia del transmisor
+%Gr= ganancia del receptor
+%L= factor de perdida 
+%lambda= longitud de onda en metros
+%
+%fuente: Ecuación 3.1 Rappaport- Wireless Communications
+
+% Maria Fernanda Peña 2021- Radioenlaces
+
+
+function [Pr]=potencia(d,Pt,Gt,Gr,L,lambda)
+    if nargin ~= 6
+      help Pr;
+      return;
+    end
+    Pr=(Pt*Gt*Gr*(lambda.^2))./ (((4*pi).^2)*(d.^2)*L);
