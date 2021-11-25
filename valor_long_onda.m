c2da17952bc61ef028465b31560c9a2bc5ffcd02
AFC7 <93738429+AFC7@users.noreply.github.com>
2021-11-25 14:55:31 -0500

    encontrar el valor de la longitud de onda

---
+++ valor_long_onda.m
@@ -0,0 +1,13 @@
+%lambda.m - find value of wavelength/encontrar el valor de la longitud de onda
+%
+%so:
+% y = value of wavelength
+%
+% f = Frequency
+%
+%Sergio A. Castro - 2021 - Universidad de Cundinamarca
+function y=lambda(f)
+c=3*10^8;
+y=c/f;
+
+end
