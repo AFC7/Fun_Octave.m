1d26a57fb4be3de0381a68a992a0f1dd93de1702
AFC7 <93738429+AFC7@users.noreply.github.com>
2021-11-25 14:30:30 -0500

    coeficiente de reflexion en paralelo

---
+++ coeficiente_de_reflx_en_paralelo.m
@@ -0,0 +1,23 @@
+%ReflexPa.m- coeficiente de reflexion en paralelo 
+%
+% uso: function Re=ReflexPa(n1,n2,Tet,Tei)
+%      
+%
+%     Re- coeficiente de reflexion en paralelo
+%     n1- indice de refraccion 
+%     n2- indice de refraccion 
+%     Tet- angulo en Et
+%     Tei- angulo en Ei 
+%  
+%Re=(n2*sind(Tet)-n1*sind(Tei))/(n2*sind(Tet)+n1*sind(Tei)) entrega un valor en grados
+% 
+%fuente: Ecuación 3.19 Rappaport- Wireless Communications
+
+% Sebastián Barrera Triana 2021- Radio enlaces 
+
+function Re=ReflexPa(n1,n2,Tet,Tei)
+      if nargin ~= 4,
+      help ReflexPa;
+      return;
+    end
+    Re=((n2*sind(Tet))-(n1*sind(Tei)))/((n2*sind(Tet))+(n1*sind(Tei)));
