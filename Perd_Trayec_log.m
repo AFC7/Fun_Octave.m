c3cf73c5aad1afa4233bf6b5a7ac90a4f5c44cae
AFC7 <93738429+AFC7@users.noreply.github.com>
2021-11-25 14:42:35 -0500

    Update Perd_Trayec_log.m

--- Perd_Trayec_log.m
+++ Perd_Trayec_log.m
@@ -5,13 +5,13 @@
 %PLdB- perdida de trayectoria entre transmisor y receptor en dB
 %PLd0- perdida de trayectoria dada en dB en la distancia de referencia.
 %d0- distancia de referencia en metros.
-%n-  exponente de perdida trayectoria segun el medio de transmisión.
+%n-  exponente de perdida trayectoria segun el medio de transmisiÃ³n.
 %d- distancia entre transmisor y receptor en metros.
 %
 %PLdB= (PLd0+(10*n*log10(d/d0)))
 %
-%recopilación de la fuente: ecuación 3.68 Rappaport-wireless communications
-
+%recopilaciÃ³n de la fuente: ecuaciÃ³n 3.68 Rappaport-wireless communications
+%
 %realizado por Marco Erazo Gomez 2021-radioenlaces
 
 function PLdB=PerdTrayeclog(PLd0,d0,n,d)
