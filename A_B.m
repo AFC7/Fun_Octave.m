209417e75e01d07d9f14aad18539ac7941fab05e
AFC7 <93738429+AFC7@users.noreply.github.com>
2021-12-01 10:24:00 -0500

    Angulo de Brewster
    para polarización vertical de una onda que incide en el suelo, para una permitividad dada (Er)

---
+++ A_B.m
@@ -0,0 +1,18 @@
+% A_B.m - Angulo de Brewster para polarización vertical de una onda que incide en el suelo, para una permitividad dada (Er)
+%
+% uso: angulodeg = ab(Er)
+%
+% A_B =  Angulo de Brewster para polarización vertical de una onda que incide en el suelo, para una permitividad dada (Er)
+% Er = permitividad relativa 
+%
+% fuente : 3.5.2 Brewster Angle, captulo 3 Rappaport - Wireless Communications
+%
+% Brandon Giovanny Cruz Rodriguez 2021 - Radioenlaces
+
+
+function angulodeg = A_B(Er)
+   frac = sqrt(Er-1)/sqrt(Er.^2-1)
+   angulorad = asin(frac)
+   angulodeg = angulorad * (180/pi)
+  
+  endfunction 
