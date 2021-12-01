9ee4d1db93ce3b9eeadf1cb3f972481ad43e8499
AFC7 <93738429+AFC7@users.noreply.github.com>
2021-12-01 12:29:23 -0500

    Potencia recibida
    Potencia recibida en funcion de la distancia en el espacio libre

---
+++ P_R.m
@@ -0,0 +1,22 @@
+% P_R(d) Potencia recibida en funcion de la distancia en el espacio libre
+%
+%usage: Pr = (d,Pt,Gt,Gr,L,lambda)
+%
+%d= distancia en metros transmisor receptor 
+%Pt= potencia del transmisor
+%Gt= ganancia del transmisor
+%Gr= ganancia del receptor
+%L= factor de perdida 
+%lambda= longitud de onda en metros
+%
+%fuente: Ecuación 3.1 Rappaport- Wireless Communications
+
+% Maria Fernanda Peña 2021- Radioenlaces
+
+
+function [Pr]=P_R(d,Pt,Gt,Gr,L,lambda)
+    if nargin ~= 6
+      help P_R;
+      return;
+    end
+    Pr=(Pt*Gt*Gr*(lambda.^2))./ (((4*pi).^2)*(d.^2)*L);
