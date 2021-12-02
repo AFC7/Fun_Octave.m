80c3738b762390342d60bbc1c3cdd5903266265c
AFC7 <93738429+AFC7@users.noreply.github.com>
2021-12-02 10:01:09 -0500

    Watts convertida a dBm
    Potencia transmitida en Watts convertida a dBm

--- dBm.m
+++ dBm.m
@@ -1,12 +1,12 @@
 %pt.m - Potencia transmitida en Watts convertida a dBm 
-%Uso: dBm = pt(w)
+%Uso: pt = dBm(w)
 %w= La potencia transmitida en Watts
-%dBm= Potencia transmitida convertida a dBm 
+%pt= Potencia transmitida convertida a dBm 
 %Fuente : ejemplo 3.2 captulo 3 Rappaport - Wireless Communications
 
 %Lizeth Zamara Huertas Fajarado Radioenlaces 2021-II
 
-function dBm=pt(w)
+function pt=dBm(w)
    
   if nargin != 1 
    
@@ -14,4 +14,4 @@ function dBm=pt(w)
      return;
   end
 
-  dBm=10*log10(w*1000);
+  dBm=10*log10(w*1000)
