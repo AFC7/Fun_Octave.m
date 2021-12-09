9ff7db0d0d8e5ac5d59b248c9b32920336ac8cdd
AFC7 <93738429+AFC7@users.noreply.github.com>
2021-12-09 14:03:34 -0500

    Coeficiente de Reflexión
    Coeficiente de Reflexión en Perpendicular

---
+++ ReflePer.m
@@ -0,0 +1,14 @@
+%ReflePer.m - Coeficiente de Reflexión en Perpendicular
+%Uso: function Rp=ReflePer(Er,Thi)
+%Rp = Coeficiente de reflexión perpendicular
+%Er = Permitividad Relativa
+%Thi = Angulo de Thetai
+%Fuente: Ecuación 3.24 Rappaport - Wireless Communications
+%Realizado por: David Ricardo Ramos Varón - Radioenlaces - 2021
+
+function Rp=ReflePer(Er,Thi)
+  help ReflePer;
+  if nargin ~=2,
+    return;
+  end
+  Rp=((-Er*sind(Thi))+(sqrt(Er-(cosd(Thi)^2))))/((Er*sind(Thi))+(sqrt(Er-(cosd(Thi)^2))));
