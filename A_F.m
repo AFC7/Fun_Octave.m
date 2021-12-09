78512e15ae5a9c67f748d0f5a11c3171981a433f
AFC7 <93738429+AFC7@users.noreply.github.com>
2021-12-09 14:11:22 -0500

    Area Efectiva de Friss

---
+++ A_F.m
@@ -0,0 +1,14 @@
+%A_F.m - Area Efectiva de Friss
+%Uso: function G=AreaFriss(A,Lambda)
+%G = Ganancia de la antena del transmisor
+%A = Apertura efectiva
+%Lambda = Longitud de onda
+%Fuente: Ecuacion Area Efectiva de Friss Rappaport - Wireless Communications
+%Realizado por: Sebastian Olaya Romero - Radioenlaces - 2021
+
+function G=A_F(A,Lambda);
+  help A_F;
+  if nargin ~=2,
+    return;
+  end
+  G=(4*pi*A)/(Lambda^2);
